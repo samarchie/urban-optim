629c59e6cca8abd0555790387da4026bac9398a7
smarchie99 <67818785+smarchie99@users.noreply.github.com>
2020-07-05 21:15:49 +1200

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,16 @@
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+os.chdir(r"C:\Users\samwa\OneDrive - University of Canterbury\ENCN493 - Project")
+print(["Directory List:"] + os.listdir())
+os.chdir("Data")
+print(os.listdir())
+data = gpd.read_file("city.shp")
+
+%matplotlib qt
+data.plot()
+
+import m
