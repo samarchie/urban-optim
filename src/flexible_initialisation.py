91afd697d4a33407e579d4a72c397b6d53b1540f
Fleming171 <fleming171@mail.com>
2020-11-18 19:08:38 +1300

    making progress on flexible inputs but it's hometime now

--- src/flexible_initialisation.py
+++ src/flexible_initialisation.py
@@ -80,10 +80,11 @@ def get_parameters():
         Theme for the GUI windows
     """
 
-    settings_file = 'src/settings.cfg'
+
     weighting_options = ['Balanced', 'Hazard Denier', 'EQ Survivor', 'CC Enthusiast', 'Compact City']
 
-    default_settings = {'parents': 10,
+    parameters_file = 'src/parameters.cfg'
+    default_parameters = {'parents': 10,
                         'generations': 10,
                         'crossover probability': 0.7,
                         'mutation probability': 0.2,
@@ -96,8 +97,7 @@ def get_parameters():
                         'max density': 140,
                         'step size': 10,
                         'theme': sg.theme()}
-
-    settingsKeys_to_elementKeys = {'parents': '-PARENTS-',
+    parametersKeys_to_elementKeys = {'parents': '-PARENTS-',
                                 'generations': '-GENERATIONS-',
                                 'crossover probability': '-CX_PROB-',
                                 'mutation probability': '-MUT_PROB-',
@@ -110,6 +110,93 @@ def get_parameters():
                                 'max density': '-MAX_DENS-',
                                 'step size': '-STEP_SIZE-',
                                 'theme': '-THEME-'}
+
+    city_info_file = 'src/city_info.cfg'
+    default_city_info = {}
+    cityinfoKeys_to_elementKeys = {}
+
+    objectives_file = 'scr/objectives.cfg'
+    default_objectives = {}
+    objectiveKeys_to_elementKeys = {}
+
+    settings_file = 'src/settings.cfg'
+    default_settings = {'theme': sg.theme(),
+                        'margins': [50, 50]}
+    settingsKeys_to_elementKeys = {'theme': '-THEME-',
+                                'margins': '-MARGINS-'}
+
+    ########## Load/Save Parameters File ##########
+    def load_parameters(parameters_file, default_parameters):
+        try:
+            with open(parameters_file, 'r') as f:
+                parameters = jsonload(f)
+        except Exception as e:
+            sg.popup_quick_message(f'exception {e}', 'No parameters file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+            parameters = default_parameters
+            save_parameters(parameters_file, parameters, None)
+        return parameters
+
+    def save_parameters(parameters_file, parameters, values):
+        if values:      # if there are stuff specified by another window, fill in those values
+            for key in parametersKeys_to_elementKeys:  # update window with the values read from parameters file
+                try:
+                    parameters[key] = values[parametersKeys_to_elementKeys[key]]
+                except Exception as e:
+                    print(f'Problem updating parameters from window values. Key = {key}')
+
+        with open(parameters_file, 'w') as f:
+            jsondump(parameters, f)
+
+        sg.popup('Parameters saved')
+
+    ########## Load/Save City Information File ##########
+    def load_city_info(city_info_file, default_city_info):
+        try:
+            with open(city_info_file, 'r') as f:
+                city_info = jsonload(f)
+        except Exception as e:
+            sg.popup_quick_message(f'exception {e}', 'No city_info file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+            city_info = default_city_info
+            save_parameters(city_info_file, city_info, None)
+        return city_info
+
+    def save_city_info(city_info_file, city_info, values):
+        if values:      # if there are stuff specified by another window, fill in those values
+            for key in cityinfoKeys_to_elementKeys:  # update window with the values read from parameters file
+                try:
+                    city_info[key] = values[cityinfoKeys_to_elementKeys[key]]
+                except Exception as e:
+                    print(f'Problem updating city_info from window values. Key = {key}')
+
+        with open(city_info_file, 'w') as f:
+            jsondump(city_info, f)
+
+        sg.popup('City Information saved')
+
+    ########## Load/Save Objectives File ##########
+    def load_objectives(objectives_file, default_objectives):
+        try:
+            with open(objectives_file, 'r') as f:
+                objectives = jsonload(f)
+        except Exception as e:
+            sg.popup_quick_message(f'exception {e}', 'No objectives file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+            objectives = default_objectives
+            save_parameters(objectives_file, objectives, None)
+        return objectives
+
+    def save_objectives(objectives_file, objectives, values):
+        if values:      # if there are stuff specified by another window, fill in those values
+            for key in objectivesKeys_to_elementKeys:  # update window with the values read from objectives file
+                try:
+                    objectives[key] = values[objectivesKeys_to_elementKeys[key]]
+                except Exception as e:
+                    print(f'Problem updating objectives from window values. Key = {key}')
+
+        with open(objectives_file, 'w') as f:
+            jsondump(objectives, f)
+
+        sg.popup('Objectives saved')
+
 
     ########## Load/Save Settings File ##########
     def load_settings(settings_file, default_settings):
@@ -135,18 +222,18 @@ def get_parameters():
 
         sg.popup('Settings saved')
 
-    ##################### Make a settings window #####################
-    def create_settings_window(settings, weighting_options):
+    ##################### Make sub windows #####################
+    def create_parameters_window(parameters, weighting_options, settings):
         sg.theme(settings['theme'])
 
-        def TextLabel(text): return sg.Text(text+':', justification='r', size=(15,1))
+        def TextLabel(text): return sg.Text(text+':', justification='r', size=(25,1))
 
-        layout = [  [sg.Text('Settings', font='Any 15')],
+        layout = [  [sg.Text('Parameters', font='Any 15')],
                     [TextLabel('Parents'), sg.In(key='-PARENTS-')],
                     [TextLabel('Generations'), sg.In(key='-GENERATIONS-')],
-                    [TextLabel('crossover probability'), sg.In(key='-CX_PROB-')],
-                    [TextLabel('mutation probability'), sg.In(key='-MUT_PROB-')],
-                    [TextLabel('individual mutation probability'), sg.In(key='-IND_MUT_PROB-')],
+                    [TextLabel('Crossover Probability'), sg.In(key='-CX_PROB-')],
+                    [TextLabel('Mutation Probability'), sg.In(key='-MUT_PROB-')],
+                    [TextLabel('Individual Mutation Probability'), sg.In(key='-IND_MUT_PROB-')],
                     [TextLabel('Weightings'), sg.Combo(weighting_options, key='-WEIGHTINGS-')    ],
                     [TextLabel('Required Dwellings'), sg.In(key='-DWELLINGS-')],
                     [TextLabel('Dwelling Scheme'), sg.In(key='-D_SCHEME-')],
@@ -154,71 +241,116 @@ def get_parameters():
                     [TextLabel('Minimum Allowable Density'), sg.In(key='-MIN_DENS-')],
                     [TextLabel('Maximum Allowable Density'), sg.In(key='-MAX_DENS-')],
                     [TextLabel('Spatial Plan Plotting Step Size'), sg.In(key='-STEP_SIZE-')],
-                    [TextLabel('Theme'), sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
                     [sg.Button('Save'), sg.Button('Exit')]  ]
 
-        window = sg.Window('Settings', layout, keep_on_top=True, finalize=True)
+        window = sg.Window('Parameters', layout, margins=(50, 50), keep_on_top=True, finalize=True)
 
-        for key in settingsKeys_to_elementKeys:   # update window with the values read from settings file
+        for key in parametersKeys_to_elementKeys:   # update window with the values read from parameters file
             try:
-                window[settingsKeys_to_elementKeys[key]].update(value=settings[key])
+                window[parametersKeys_to_elementKeys[key]].update(value=parameters[key])
             except Exception as e:
-                print(f'Problem updating PySimpleGUI window from settings. Key = {key}')
+                print(f'Problem updating PySimpleGUI window from parameters. Key = {key}')
 
         return window
 
-    ##################### Main Program Window #####################
-    def create_main_window(settings):
+
+    # def create_city_info_window(city_info, settings):
+    #     sg.theme(settings['theme'])
+    #
+    #     def TextLabel(text): return sg.Text(text+':', justification='r', size=(25,1))
+    #
+    #     layout = [  [sg.Text('City Information', font='Any 15')],
+    #                 [TextLabel('Parents'), sg.In(key='-PARENTS-')],
+    #                 [sg.Button('Save'), sg.Button('Exit')]  ]
+    #
+    #     window = sg.Window('City Information', layout, margins=(50, 50), keep_on_top=True, finalize=True)
+    #
+    #     for key in parametersKeys_to_elementKeys:   # update window with the values read from parameters file
+    #         try:
+    #             window[parametersKeys_to_elementKeys[key]].update(value=parameters[key])
+    #         except Exception as e:
+    #             print(f'Problem updating PySimpleGUI window from parameters. Key = {key}')
+    #
+    #     return window
+
+    settings = load(settings_file, default_settings)
+    def create_settings_window(settings):
         sg.theme(settings['theme'])
 
+        def TextLabel(text): return sg.Text(text+':', justification='r', size=(25,1))
+
+        layout = [  [sg.Text('Settings', font='Any 15')],
+                    [TextLabel('Theme'), sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
+                    [TextLabel('Margins'), sg.In(key='-MARGINS-')],
+                    [sg.Button('Save'), sg.Button('Exit')]  ]
+
+        window = sg.Window('Settings', layout, margins=settings['margins'], keep_on_top=True, finalize=True)
+
+        for key in settingsKeys_to_elementKeys:   # update window with the values read from parameters file
+            try:
+                window[settingsKeys_to_elementKeys[key]].update(value=settings[key])
+            except Exception as e:
+                print(f'Problem updating PySimpleGUI window from parameters. Key = {key}')
+
+        return window
+
+
+    ##################### Main Program Window #####################
+    def create_main_window(parameters):
+        sg.theme(parameters['theme'])
+
         layout = [[sg.T('This is my main application')],
-                  [sg.T('Add your primary window stuff in here')],
-                  [sg.B('Ok'), sg.B('Change Settings')]]
+                  [sg.B('Change Parameters')],
+                  [sg.B('Change City Data')],
+                  [sg.B('Change Objectives')],
+                  [sg.B('Ok')]]
 
         return sg.Window('Main Application', layout)
 
     ##################### Clean Up #####################
-    def tidy(settings):
+    def tidy(parameters):
         densities = []
-        for bit in settings['densities'].split(','):
-            if '(' in bit:
+        for bit in parameters['densities'].split(','):
+            if '(' or '[' in bit:
                 bit = bit[1:]
             if ' ' in bit:
                 bit = bit[1:]
-            if ')' in bit:
+            if ')' or '[' in bit:
                 bit = bit[:-1]
             densities.append(float(bit))
-        settings['densities'] = densities
+        parameters['densities'] = densities
 
-        return settings
+        return parameters
 
     ##################### Event Loop #####################
     def run():
-        window, settings = None, load_settings(settings_file, default_settings )
+        window = None
+        parameters = load(parameters_file, default_parameters)
+        settings = load(settings_file, default_settings)
 
         while True:             # Event Loop
             if window is None:
-                window = create_main_window(settings)
+                window = create_main_window(parameters)
 
             event, values = window.read()
             if event in (sg.WIN_CLOSED, 'Ok'):
                 break
-            if event == 'Change Settings':
-                event, values = create_settings_window(settings, weighting_options).read(close=True)
+            if event == 'Change Parameters':
+                event, values = create_parameters_window(parameters, weighting_options, settings).read(close=True)
                 if event == 'Save':
                     window.close()
                     window = None
-                    save_settings(settings_file, settings, values)
+                    save_parameters(parameters_file, parameters, values)
         window.close()
 
-        settings = tidy(settings)
+        parameters = tidy(parameters)
 
-        return settings
+        return parameters
 
-    settings = run()
+    parameters = run()
 
-    scheme = settings['weightings'] + ', ' + settings['dwelling scheme']
+    scheme = parameters['weightings'] + ', ' + parameters['dwelling scheme']
 
-    return int(settings['parents']), int(settings['generations']), float(settings['crossover probability']), float(settings['mutation probability']), float(settings['individual mutation probability']), settings['weightings'], int(settings['required dwellings']), scheme, settings['densities'], float(settings['min density']), float(settings['max density']), int(settings['step size']), settings['theme']
+    return int(parameters['parents']), int(parameters['generations']), float(parameters['crossover probability']), float(parameters['mutation probability']), float(parameters['individual mutation probability']), parameters['weightings'], int(parameters['required dwellings']), scheme, parameters['densities'], float(parameters['min density']), float(parameters['max density']), int(parameters['step size']), parameters['theme']
 
 NO_parents, NO_generations, prob_crossover, prob_mutation, prob_mut_indiv, weightings, required_dwellings, scheme, density_total, min_density_possible, max_density_possible, when_to_plot, theme = get_parameters()
